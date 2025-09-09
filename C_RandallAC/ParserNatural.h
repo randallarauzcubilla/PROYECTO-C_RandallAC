c46225c42534f666aed37033fe657f110a3d9d3d
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-09-08 23:57:58 -0600

    Update parser interface for extended semantic recognition.

--- C_RandallAC/ParserNatural.h
+++ C_RandallAC/ParserNatural.h
@@ -1,7 +1,16 @@
 #pragma once
-using namespace System;
 
-ref class ParserNatural {
+#define MAX_LINEA 256
+#include "DiccionarioManual.h"
+
+class ParserNatural {
 public:
-    static String^ ParseLinea(String^ linea);
+    ParserNatural();
+
+    // Traduce línea natural a C++
+    void parseLinea(const char* lineaEntrada, char* lineaSalida);
+
+private:
+    bool contienePalabra(const char* linea, const char* palabra);
+    void reemplazarPalabra(char* linea, const char* buscar, const char* reemplazar);
 };
