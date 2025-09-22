649a36a18dde681ded98e811b6b0993eb83eb867
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-09-22 17:02:29 -0600

    GeneradorCodigo.h: Declared new generation methods for controlled repetition and output.

--- C_RandallAC/GeneradorCodigo.h
+++ C_RandallAC/GeneradorCodigo.h
@@ -1,6 +1,8 @@
-#define MAX_LINEA 256
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_LINEA_GENERADOR 256
 struct NodoCodigo {
-    char linea[MAX_LINEA];
+    char linea[MAX_LINEA_GENERADOR];
     NodoCodigo* siguiente;
 };
 
@@ -8,17 +10,18 @@ class GeneradorCodigo {
 private:
     NodoCodigo* cabeza;
     NodoCodigo* cola;
-
-    bool contiene(const char* texto);
+    NodoCodigo* cabezaFueraMain;
+    NodoCodigo* colaFueraMain;
 
 public:
     GeneradorCodigo();
     ~GeneradorCodigo();
-
+    bool contiene(const char* texto);
     void agregar(const char* instruccionCpp, bool indentar);
     void finalizar();
     void imprimirCodigo();  // útil para debug
     void generarArchivo(const char* nombreArchivo);
     void limpiar();
     void agregarFueraMain(const char* instruccionCpp);
+    void  generarLlamadaFuncion(const char* nombre, const char* parametrosRaw, char* llamadaCpp);
 };
